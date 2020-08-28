47580e6edb4d3bce05c2d017ca1ad2f958f3956e
abokhaddr <69322318+abokhaddr@users.noreply.github.com>
2020-08-29 00:36:05 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+const express = require('express');
+const app = new express();
+
+app.set('views',path.join(__dirname, '/views'));
+app.set('view engine','ejs');
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', (req, res) => res.render('index', { title: 'Index' }));
+
+app.listen(3000, () => { console.log('PS Project Running on port 3000!') }); 
+<%- include('header') %>
+
+  <div class="container">
+    <h1><%= title %></h1>
+    <a href='/profile'>Profile</a>
+  </div>
+
+  <br><a href='/transfer'>Transfer</a>
+
+<%- include('footer') %
