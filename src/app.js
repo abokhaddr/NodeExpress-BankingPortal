8f8336f8d69276c7366cec098842aff1f8ff4466
abokhaddr <69322318+abokhaddr@users.noreply.github.com>
2020-08-29 01:02:55 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,20 +9,6 @@ app.set('view engine','ejs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.get('/', (req, res) => res.render('index', { title: 'Index' }));
-
-app.listen(3000, () => { console.log('PS Project Running on port 3000!') });
-const fs = require('fs');
-const path = require('path');
-
-const express = require('express');
-const app = new express();
-
-app.set('views',path.join(__dirname, '/views'));
-app.set('view engine','ejs');
-
-app.use(express.static(path.join(__dirname, '/public')));
-
 const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
 const accounts = JSON.parse(accountData);
 
