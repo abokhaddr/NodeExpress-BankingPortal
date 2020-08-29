c026ce5ea7ea6f3e16731afd37b5d9660c25a673
abokhaddr <69322318+abokhaddr@users.noreply.github.com>
2020-08-30 02:57:10 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ app.get('/payment', (req, res) => res.render('payment', {account: accounts.credi
 app.post('/payment', (req, res) => {
     accounts.credit.balance -= req.body.amount;
     accounts.credit.available += parseInt(req.body.amount);
-    let accountsJSON = JSON.stringify(accounts, null, 4)
+    let accountsJSON = JSON.stringify(accounts, null, 4);
     fs.writeFileSync(path.join(__dirname, 'json','accounts.json'), accountsJSON, 'utf8');
     res.render('payment', {message: 'Payment Successful', account: accounts.credit});
 });
