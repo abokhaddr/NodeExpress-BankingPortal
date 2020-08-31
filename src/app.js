058e0a94ed0c9f2a8f1d09fc7cc100153086d757
abokhaddr <69322318+abokhaddr@users.noreply.github.com>
2020-08-31 03:51:40 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -22,4 +22,4 @@ app.use('/services', servicesRoutes);
 
 app.get('/profile', (req, res) =>  res.render('profile', { user: users[0] }));
 
-app.listen(3000, () => { console.log('PS Project Running on port 3000!') });
+app.listen(3000, () => { console.log('PS Project Running on port 3000!') }); 
