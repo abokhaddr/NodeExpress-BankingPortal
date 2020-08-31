86dbfdb49c040b01d5a5e6ef2e3e51699ec20192
abokhaddr <69322318+abokhaddr@users.noreply.github.com>
2020-08-31 23:45:00 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -22,22 +22,7 @@ app.use('/services', servicesRoutes);
 
 
 
-app.get('/transfer', (req, res) =>  res.render('transfer'));
 
-app.post('/transfer', (req, res) => {
-    accounts[req.body.from].balance -= req.body.amount;
-    accounts[req.body.to].balance += parseInt(req.body.amount, 10);
-    writeJSON();
-    res.render('transfer', {message: 'Transfer Completed'});
-});
-
-app.get('/payment', (req, res) => res.render('payment', {account: accounts.credit}));
-app.post('/payment', (req, res) => {
-    accounts.credit.balance -= req.body.amount;
-    accounts.credit.available += parseInt(req.body.amount);
-    writeJSON();
-    res.render('payment', {message: 'Payment Successful', account: accounts.credit});
-});
 
 app.get('/profile', (req, res) =>  res.render('profile', { user: users[0] }));
 
