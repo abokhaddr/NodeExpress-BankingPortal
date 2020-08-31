b117c3816961b2f1ac12e93d36512b6ecc21b117
abokhaddr <69322318+abokhaddr@users.noreply.github.com>
2020-08-31 23:41:35 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -20,9 +20,7 @@ app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accou
 app.use('/account', accountRoutes);
 app.use('/services', servicesRoutes);
 
-app.get('/savings', (req, res) => res.render('account', { account: accounts.savings }));
-app.get('/checking', (req, res) => res.render('account', { account: accounts.checking }));
-app.get('/credit', (req, res) => res.render('account', { account: accounts.credit }));
+
 
 app.get('/transfer', (req, res) =>  res.render('transfer'));
 
